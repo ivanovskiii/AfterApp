5530df3e72e71bf0130a40e081ab388c9359c892
Gorjan Ivanovski 183092 <gorjan.ivanovski@students.finki.ukim.mk>
2023-04-30 22:59:44 +0200

    Initial Commit

---
+++ AfterApp/AfterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfterAppApp.swift
+//  AfterApp
+//
+//  Created by Gorjan Ivanovski on 30.4.23.
+//
+
+import SwiftUI
+
+@main
+struct AfterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
